5217f8c1026cef1518e22c884a09021045b2756e
Honza Javorek <mail@honzajavorek.cz>
2018-09-17 20:28:25 +0200

    add forgotten script

---
+++ scripts/lint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Make sure requirements.txt are in sync with Pipfile.lock
+pipenv run pipenv_to_requirements -f -o ci-requirements.txt
+if ! diff requirements.txt ci-requirements.txt > /dev/null
+then
+  help_url='https://cojeapi.readthedocs.io/cs/latest/CONTRIBUTING.html#zavislosti'
+  echo ""
+  echo ""
+  echo "The requirements.txt file is not up to date. Run" \
+    "'pipenv run requirements' and commit the changes." \
+    "See ${help_url} for more info."
+  exit 1
+fi
