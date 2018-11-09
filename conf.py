787966c57aa79f5716d8b862e5ed2d558da31ac5
Honza Javorek <mail@honzajavorek.cz>
2018-11-09 17:08:49 +0100

    hide todos

--- conf.py
+++ conf.py
@@ -187,4 +187,4 @@ sphinx_tabs_valid_builders = ['linkcheck']
 # -- Options for todo extension ----------------------------------------------
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = True
+todo_include_todos = False
